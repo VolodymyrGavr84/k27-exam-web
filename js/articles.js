52f8d2cf8e0941f2e3239a6f82558003fb4b9d65
Volodymyr Gavrylov <gavrylov84@yahoo.com>
2022-06-16 13:36:46 +0300

    add pages about,articles+css,js files

---
+++ js/articles.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// sticky sideBar
+let SideBar = document.querySelector('.js-sidebar');
+
+window.onscroll = function() {
+if (window.pageYOffset > 300) {
+    SideBar.classList.add('js-sidebar--shown') 
+} else {
+    SideBar.classList.remove('js-sidebar--shown')
+}
+};
--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,12 @@
 'use strict';
 
-// header email-form 
-let sendEmail = document.querySelector('.header-email');
+// header email-form visible
+let sendEmail = document.querySelector('.email-header');
 let sendFormHidden = document.querySelector('.email-form');
 let hideForm = document.querySelector('.email-form__close-icon');
+
 sendEmail.onclick = function() {
-sendFormHidden.classList.add('js-email-form--visible');
+sendFormHidden.classList.toggle('js-email-form--visible');
 };
 hideForm.onclick = function() {
 sendFormHidden.classList.remove('js-email-form--visible');
