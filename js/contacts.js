c0e07044ebb6714670f2ff26fc05a665e1f09dd8
Volodymyr Gavrylov <gavrylov84@yahoo.com>
2022-07-02 23:30:21 +0300

    fixed resolution>no content goes more then 1920px

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // header email-form visible
-let sendEmail = document.querySelector('.header-email');
+let sendEmail = document.querySelector('.email-btn');
 let sendFormHidden = document.querySelector('.email-form');
 let hideForm = document.querySelector('.email-form__close-icon');
 
