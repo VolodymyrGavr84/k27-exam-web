bde4d2fadac10e89502ed52c91dcfc5eaeff49a0
Volodymyr Gavrylov <gavrylov84@yahoo.com>
2022-07-18 16:52:37 +0300

    finished @media adaptation for products and gallery pages

--- js/products.js
+++ js/products.js
@@ -7,6 +7,26 @@ $(document).ready(function(){
         initialSlide: 7,
         slidesToShow: 5,
         slidesToScroll: 1,
-        lazyLoad: 'progressive'
+        lazyLoad: 'progressive',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+            {
+            breakpoint:800,
+            settings: {
+                slidesToShow:3,
+            }
+        },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+        ]
     });
 });
